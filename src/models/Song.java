a67e4cdce8de14dfc457a0f6a2ba5fa77806bca3
silviaillescas <silviaillesca402@gmail.com>
2022-11-13 15:11:35 -0600

    se completo el modelo cancion.

--- src/models/Song.java
+++ src/models/Song.java
@@ -5,10 +5,67 @@
  */
 package models;
 
+import interfaces.ISong;
+
 /**
  *
  * @author Silvia
  */
-public class Song {
+public class Song implements ISong {
+    String title;
+    String artist;
+    String album;
+    String duration;
+    String genre;
+    
+    @Override
+    public void setTitle(String _title) {
+        this.title = _title;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public void setArtist(String _artist) {
+        this.artist = _artist;
+    }
+
+    @Override
+    public String getArtist() {
+        return artist;
+    }
+
+    @Override
+    public void setAlbum(String _album) {
+        this.album =_album;
+    }
+
+    @Override
+    public String getAlbum() {
+        return this.album;
+    }
+
+    @Override
+    public void setDuration(String _duration) {
+        this.duration =_duration;
+    }
+
+    @Override
+    public String getDuration() {
+        return this.duration;
+    }
+
+    @Override
+    public void setGenre(String _genre) {
+        this.genre = _genre;
+    }
+
+    @Override
+    public String getGenre() {
+        return this.genre;
+    }
     
 }
